7f970aefd27d956535462d88a0a1818d120e6cb0
Maurice Thompson-Hamilton <mauricemlth@hotmail.com>
2023-03-17 11:38:24 +0000

    disabled memory pooling and added commandline argument parser

--- RayTracerSmall/main.cpp
+++ RayTracerSmall/main.cpp
@@ -46,9 +46,56 @@
 //#define INFINITY 1e8
 //#endif
 
+enum RenderMode { Basic, Simple, Smooth };
+
+RenderMode _eMode;
+
 float mix(const float &a, const float &b, const float &mix)
 {
 	return b * mix + a * (1 - mix);
+}
+
+static void ShowUsage(std::string name) {
+	std::cout 
+		<< "\nUsage: " << name << " [options]" 
+		<< "\n/ or ?:\tdisplays this information."
+		<< "\n--json:\tload setttings from JSON file" 
+		<< "\n--basic:\tBasicRender"
+		<< "\n--simple:\tSimpleShrinking" 
+		<< "\n--smooth:\tSmoothScaling (default)" 
+		<< "\n\nExample:\n\t"
+		<< name << " --json settings.json --basic" 
+		<< std::endl;
+}
+
+bool ParseArgs(int argc, char** argv)
+{
+	std::string sFile;
+
+	for (int i = 0; i < argc; i++)
+	{
+		if (argv[i] == "/" ||
+			argv[i] == "?")
+			return false;
+		
+		if (argv[i] == "--json")
+		{
+			sFile = argv[i + 1];
+			if (sFile.find_last_of(".json") < 1)
+				return false;
+		}
+
+		if (argv[i] == "--basic")
+			_eMode = RenderMode::Basic;
+
+		if (argv[i] == "--simple")
+			_eMode = RenderMode::Simple;
+
+		if (argv[i] == "--smooth")
+			_eMode = RenderMode::Smooth;
+	}
+
+	return true;
 }
 
 //[comment]
@@ -61,13 +108,34 @@ int main(int argc, char **argv)
 	MemoryManager::init();
 	MemoryManager::enablePooling(false);
 
+	_eMode = RenderMode::Smooth;
+	
+	if (argc < 2) {
+		std::cout
+			<< "\nNo settings JSON specified." 
+			<< "\nDefault to using Smooth Scaling method."
+			<< std::endl;
+	}
+	
+	if (!ParseArgs(argc, argv)) 
+	{
+		ShowUsage(argv[0]);
+		return 1;
+	}
+	
+
 	auto tracer = new Tracer();
 	
 	// This sample only allows one choice per program execution. Feel free to improve upon this
 	srand(13);
-	//BasicRender();
-	//SimpleShrinking();
-	tracer->SmoothScaling();
+	
+	switch (_eMode)
+	{
+		case Basic: tracer->BasicRender(); break;
+		case Simple: tracer->SimpleShrinking(); break;
+		case Smooth:
+		default: tracer->SmoothScaling(); break;
+	}
 
 	return 0;
 }
